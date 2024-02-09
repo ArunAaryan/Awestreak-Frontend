2f82fe0dd584a89c81f4cd85adc12e6de87d2ce3
arunaaryan <rnyadav98@gmail.com>
2024-02-09 23:58:06 +0530

    fix (UserSwitch Query Invalidation) : fix invalidation; reset client header

--- src/HomeTemp.tsx
+++ src/HomeTemp.tsx
@@ -1,16 +1,22 @@
+import { useQueryClient } from "react-query";
 import { useNavigate } from "react-router-dom";
+import axiosClient from "./axios";
 
 const HomeTemp = () => {
   const ARUN = "clrxmr5wj0001c5i25cgrpjqt";
   const RANJITHA = "clrz6u0n000004ks6q2bh3eiv";
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const setLocalStorageUser = async (userId: string) => {
+    queryClient.invalidateQueries();
     const user = await localStorage.setItem("currentUser", userId);
+    axiosClient.defaults.headers.Authorization = userId;
     navigate("/boards/all");
   };
   return (
     <div className="flex text-gray-100 flex-col justify-center items-center w-full gap-4">
       <div>Home Temporary</div>
+      <div>Select a User</div>
       <div className="flex gap-2 justify-center">
         <div
           className="border-2 border-gray-50 p-2 rounded-md hover:cursor-pointer"
--- src/axios.ts
+++ src/axios.ts
@@ -2,10 +2,14 @@ import axios from "axios";
 
 import { API_URL } from "../config";
 
+const getCurrentUser = () => {
+  console.log("getCurrentUser", localStorage.getItem("currentUser"));
+  return localStorage.getItem("currentUser");
+};
 const axiosClient = axios.create({
   baseURL: API_URL,
   headers: {
-    Authorization: localStorage.getItem("currentUser"),
+    Authorization: getCurrentUser(),
   },
 });
 

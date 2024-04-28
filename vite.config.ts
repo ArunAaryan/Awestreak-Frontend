14198bc2e47cf76cf9cb8f439534381b223594b5
ArunAaryan <rnyadav98@gmail.com>
2024-04-28 20:52:45 +0530

    feat (PWA): - add assets - make manifest object & inject

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,44 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+export const manifest = {
+  theme_color: "#000000",
+  background_color: "#000000",
+  display: "fullscreen",
+  scope: "/",
+  start_url: "/",
+  name: "Awestreak",
+  short_name: "Awestreak",
+  description: "Public Learning & Streaks",
+  icons: [
+    {
+      src: "/icon-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "/icon-256x256.png",
+      sizes: "256x256",
+      type: "image/png",
+    },
+    {
+      src: "/icon-384x384.png",
+      sizes: "384x384",
+      type: "image/png",
+    },
+    {
+      src: "/icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
+};
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifest)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});

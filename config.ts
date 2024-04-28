36eb9f336c5425ab33a831e7c8f010eb82075cc4
ArunAaryan <rnyadav98@gmail.com>
2024-04-28 08:56:12 +0530

    fix(change env url)

--- config.ts
+++ config.ts
@@ -2,5 +2,6 @@
 // export const API_URL = "http://192.168.1.79:3000";
 // export const API_URL = "http://100.78.221.24:3000";
 // export const API_URL = "http://100.121.53.124:3000";
-export const API_URL = "http://127.0.0.1:3000";
+// export const API_URL = "http://127.0.0.1:3000";
 //export const API_URL = "http://aruns-macbook-pro.zapus-boa.ts.net:3000";
+export const API_URL = "https://awestreak-backend.vercel.app/";
